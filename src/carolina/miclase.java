94b880adafc2c36f7313f7fc7e906d56bdce09d0
Alumno <alumno@localhost.localdomain>
2014-01-29 08:45:16 -0600

    proyecto

---
+++ src/carolina/miclase.java
@@ -0,0 +1,24 @@
+package carolina;
+
+import java.util.Scanner;
+
+public class miclase{
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		System.out.print("1");
+		System.out.print("3");
+		System.out.print("4");
+		System.out.print("5");
+		System.out.print("6");
+		Scanner mi_scanner = new Scanner(System.in);
+		int mi_variable=mi_scanner.nextInt();
+		System.out.print(mi_variable*2);
+		
+		if(mi_variable ==10){
+			System.out.print("es un 10");
+	
+		}
+	}
+	
+}
